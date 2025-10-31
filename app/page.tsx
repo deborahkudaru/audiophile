13a1c4c9306d5265e51a13bdef232de2543d748f
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-10-31 10:10:36 +0100

    landing page content

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import ProductAds from "@/components/ProductAds";
 import ProductCategory from "@/components/ProductCategory";
+import Footer from "@/components/Footer";
 
 export default function Home(){
   return(
@@ -10,6 +11,7 @@ export default function Home(){
       <Hero />
       <ProductCategory />
       <ProductAds />
+      <Footer />
     </div>
   )
 }
